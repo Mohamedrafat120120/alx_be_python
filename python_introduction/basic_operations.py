d270059177596b3afef670a6e5f6ff7271a3496e
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-09-20 02:44:18 +0300

    first task commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1=15
+number2=10
+sum=number2+number1
+subtraction=number1-number2
+multiplication=number1*number2
+print(f"Addition operation of {number1} and {number2} is {sum}")
+print(f"subtraction operation of {number1} and {number2} is {subtraction}")
+print(f"multiplication operation of {number1} and {number2} is {multiplication}")
+
