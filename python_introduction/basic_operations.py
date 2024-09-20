96e49d72d90312345603fd83d49d121366c979f6
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-09-20 20:19:05 +0300

    first task edited

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,8 @@
-number1=10
-number2=5
-sum=number2+number1
-subtraction=number1-number2
-multiplication=number1*number2
+number1 = 10
+number2 = 5
+sum = number2 + number1
+subtraction = number1 - number2
+multiplication = number1 * number2
 print(f"Addition operation of {number1} and {number2} is {sum}")
 print(f"subtraction operation of {number1} and {number2} is {subtraction}")
 print(f"multiplication operation of {number1} and {number2} is {multiplication}")
-
