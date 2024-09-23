9881d8195d2adb1a615505de0c694a8a768d9b91
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-09-23 03:09:09 +0300

    4. Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,25 +1,25 @@
-task = input("Enter your task:")
-piriority = input("Piriority(high/medium/low)")
-time_bound = input("Is it time-bound? (yes/no):")
+Task = input("Enter your Task:")
+Piriority = input("Piriority(high/medium/low)")
+Time_bound = input("Is it time-bound? (yes/no):")
 
-match piriority:
-    case _ if piriority == "High" and time_bound == "yes":
-        print(f"'{task}' This task is high priority task that requires immediate attention today! ")
+match Piriority:
+    case _ if Piriority == "High" and Time_bound == "yes":
+        print(f"'{Task}' This Task is high priority Task that requires immediate attention today! ")
         
-    case _ if piriority == "low" and time_bound == "no":
-        print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time. ")
+    case _ if Piriority == "low" and Time_bound == "no":
+        print(f"Note: '{Task}' is a low priority Task. Consider completing it when you have free time. ")
         
-    case _ if piriority == "high" and time_bound == "no":
-        print(f"Note: '{task}' This task is high priority task that requires immediate attention next days")
+    case _ if Piriority == "high" and Time_bound == "no":
+        print(f"Note: '{Task}' This Task is high priority Task that requires immediate attention next days")
         
-    case _ if piriority == "low" and time_bound == "yes":
-        print(f"Note: '{task}' This task is low priority task but time bound so requires immediate attention today!")
+    case _ if Piriority == "low" and Time_bound == "yes":
+        print(f"Note: '{Task}' This Task is low priority Task but time bound so requires immediate attention today!")
         
-    case _ if piriority == "medium" and time_bound == "yes":
-        print(f"Note: '{task}' This task has a medium priority  but time bound so requires immediate attention today!")
+    case _ if Piriority == "medium" and Time_bound == "yes":
+        print(f"Note: '{Task}' This Task has a medium priority  but time bound so requires immediate attention today!")
         
-    case _ if piriority == "medium" and time_bound == "no":
-        print(f"Note: '{task}' This task has medium priority. Consider completing it when you have free time.")
+    case _ if Piriority == "medium" and Time_bound == "no":
+        print(f"Note: '{Task}' This Task has medium priority. Consider completing it when you have free time.")
     case _ :
-        print("you must enter a task")    
+        print("you must enter a Task")    
         
