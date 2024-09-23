4f057a3be8af9a7ca51741a213175adfa6732a9a
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-09-23 23:57:44 +0300

    3. Drawing Patterns with Nested Loops and 4. Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,19 +1,19 @@
-Task = input("Enter your Task:")
-Piriority = input("Piriority(high/medium/low)")
-Time_bound = input("Is it time-bound? (yes/no):")
+task = input("Enter your task:")
+piriority = input("piriority(high/medium/low)")
+time_bound = input("Is it time-bound? (yes/no):")
 
-match Piriority:
+match piriority:
     
     case "high":
-        reminder=f"this {Task} has a high pitiority so it is require to do in the fastest time "  
+        reminder=f"this {task} has a high pitiority so it is require to do in the fastest time "  
     case "medium":
-        reminder=f"this {Task} has a medium pitiority so make it in your attention"
+        reminder=f"this {task} has a medium pitiority so make it in your attention"
     case "low":
-        reminder=f"this {Task} has a low pitiority so it is require to do in any free time"
+        reminder=f"this {task} has a low pitiority so it is require to do in any free time"
         
-if Time_bound == "yes" and Piriority =="high":
-    reminder+= f"Reminder: '{Task}' is a high priority task that requires immediate attention today!" 
+if time_bound== "yes" and piriority =="high":
+    reminder+= f"Reminder: '{task}' is a high priority task that requires immediate attention today!" 
        
-if Time_bound == "no" and Piriority =="low":
-    reminder+= f"Note: '{Task}' is a low priority task. Consider completing it when you have free time."  
+if time_bound== "no" and piriority =="low":
+    reminder+= f"Note: '{task}' is a low priority task. Consider completing it when you have free time."  
 print(reminder)              
