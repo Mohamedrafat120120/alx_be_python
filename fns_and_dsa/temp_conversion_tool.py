e97b4b699ecc4587a1c36213b65e422d635c0945
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-09-28 00:47:42 +0300

    2. Explore datetime Module

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,26 +2,24 @@ FAHRENHEIT_TO_CELSIUS_FACTOR=5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR=9/5
 
 def convert_to_celsius(fahrenheit):
-    
     global FAHRENHEIT_TO_CELSIUS_FACTOR
-    
-    print (f"{fahrenheit} °F is {fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR} °C ")
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(f"{fahrenheit} °F is {celsius:.2f} °C")
     
     
 def convert_to_fahrenheit(celsius):
-    
     global CELSIUS_TO_FAHRENHEIT_FACTOR
-    
-    print (f"{celsius} °C is {celsius * CELSIUS_TO_FAHRENHEIT_FACTOR} °F ")
+    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+    print(f"{celsius} °C is {fahrenheit:.2f} °F")
 
 temperature = int(input("Enter the temperature to convert: "))
 
-celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")   
+celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()   
 
  
 match celsius_or_fahrenheit:
     case "C":
-        convert_to_fahrenheit(temperature)
+        convert_to_celsius(temperature)
     case "F":
         convert_to_fahrenheit(temperature)
     
