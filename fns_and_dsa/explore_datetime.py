fdf5c6a1df8c42f692eca30df505838682475333
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-09-28 00:38:39 +0300

    2. Explore datetime Module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,16 +3,16 @@ from datetime import date,time
 
 def display_current_datetime():
     current_date = datetime.datetime.now()
-    return f"Current date and time: {current_date} "
+    print(f"Current date and time: {current_date} ")
 
-print(display_current_datetime())
+display_current_datetime()
 
 number_of_days = int(input("Enter a number of days: "))
 
 def calculate_future_date():
       future_date = datetime.timedelta(days=number_of_days)+datetime.date.today()
-      return f"Future date: {future_date} "
-print(calculate_future_date())      
+      print(f"Future date: {future_date} ")
+calculate_future_date()      
     
     
     
