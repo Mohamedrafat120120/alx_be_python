c143f0ef2775910329d3a939c510d2f685844d05
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-09-27 20:08:16 +0300

    0. Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1,num2,operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'division' and num2 == 0:
+         raise ValueError("Error: Division by zero is not allowed")
+    elif operation == 'division':
+        return num1 / num2
+    else:
+        return "you must choose from these operation only(+,-,*,/)"
