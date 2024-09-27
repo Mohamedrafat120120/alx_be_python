360d575cd0a4aba47b93eda6857d297bb611b458
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-09-28 00:45:05 +0300

    2. Explore datetime Module

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,12 +2,16 @@ FAHRENHEIT_TO_CELSIUS_FACTOR=5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR=9/5
 
 def convert_to_celsius(fahrenheit):
+    
     global FAHRENHEIT_TO_CELSIUS_FACTOR
+    
     print (f"{fahrenheit} °F is {fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR} °C ")
     
     
 def convert_to_fahrenheit(celsius):
+    
     global CELSIUS_TO_FAHRENHEIT_FACTOR
+    
     print (f"{celsius} °C is {celsius * CELSIUS_TO_FAHRENHEIT_FACTOR} °F ")
 
 temperature = int(input("Enter the temperature to convert: "))
