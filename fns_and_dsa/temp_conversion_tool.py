c71badf611466e3e0de488f2bf17dfaf2e9e9e4a
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-09-28 00:20:01 +0300

    3. Temperature Conversion Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,14 +1,12 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR=5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR=9/5
 
-def convert_to_celsius(fahrenheit):
+def convert_to_celsius(fahrenheit,CELSIUS_TO_FAHRENHEIT_FACTOR):
+    print (f"{fahrenheit} °F is {fahrenheit * CELSIUS_TO_FAHRENHEIT_FACTOR} °C ")
     
-    return {fahrenheit * CELSIUS_TO_FAHRENHEIT_FACTOR}
     
-    
-def convert_to_fahrenheit(celsius):
-    
-    return {celsius * FAHRENHEIT_TO_CELSIUS_FACTOR}
+def convert_to_fahrenheit(celsius,FAHRENHEIT_TO_CELSIUS_FACTOR):
+    print (f"{celsius} °C is {celsius * FAHRENHEIT_TO_CELSIUS_FACTOR} °F ")
 
 temperature = int(input("Enter the temperature to convert: "))
 
@@ -17,9 +15,9 @@ celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/
  
 match celsius_or_fahrenheit:
     case "C":
-       print(f"{temperature} °F is {convert_to_celsius(temperature)} °C")
+        convert_to_fahrenheit(temperature)
     case "F":
-        (f"{temperature} °C is {convert_to_fahrenheit(temperature)} °F")
+        convert_to_fahrenheit(temperature)
     
     case _:
         raise ValueError ("you must enter a valid temperature")        
