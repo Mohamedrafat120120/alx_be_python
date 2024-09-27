3edcca2495eac484d83e5fd367ed53a0086cee50
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-09-28 00:22:09 +0300

    3. Temperature Conversion Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,11 +1,13 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR=5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR=9/5
 
-def convert_to_celsius(fahrenheit,CELSIUS_TO_FAHRENHEIT_FACTOR):
+def convert_to_celsius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
     print (f"{fahrenheit} °F is {fahrenheit * CELSIUS_TO_FAHRENHEIT_FACTOR} °C ")
     
     
-def convert_to_fahrenheit(celsius,FAHRENHEIT_TO_CELSIUS_FACTOR):
+def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
     print (f"{celsius} °C is {celsius * FAHRENHEIT_TO_CELSIUS_FACTOR} °F ")
 
 temperature = int(input("Enter the temperature to convert: "))
