35dd18de1a69fd9c8a93d34467f3106a6644df8a
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-10-06 00:56:53 +0300

    2. Writing Unit Tests for a Simple Calculator Class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,21 +6,21 @@ class test_calculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
     
-    def test_add(self):
+    def test_addition(self):
         result=self.calc.add(4,6)
         self.assertEqual(result,10)
         
-    def test_subtract(self):
+    def test_subtraction(self):
         result=self.calc.subtract(4,2)
         self.assertEqual(result,2)
         
         
-    def test_division(self):
+    def test_multiply(self):
         result=self.calc.multiply(6,6)
         self.assertEqual(result,36)
         
         
-    def test_multiply(self):
+    def test_divide(self):
         result=self.calc.divide(4,2)
         self.assertEqual(result,2)
         
