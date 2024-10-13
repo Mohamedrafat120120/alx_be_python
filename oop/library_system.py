8749762c3c0c7bd140c94f654032fb752c31627c
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-10-14 02:07:57 +0300

    0. Dive into Python Magic Methods

---
+++ oop/library_system.py
@@ -0,0 +1,48 @@
+class Book:
+    def __init__(self, title, author):
+        self.title=title
+        self.author=author
+        
+        
+    def __str__(self) -> str:
+        return f"{self.title}, {self.author}"
+    
+    
+    
+class EBook(Book):
+    def __init__(self, title, author, file_size=int()):
+        self.title=title
+        self.author=author
+        self.file_size=file_size
+        
+        
+    def __str__(self):
+        return f"{self.title}, {self.author}, {self.file_size}"
+    
+    
+    
+class PrintBook(Book):
+    
+     def __init__(self, title, author, pages=int()):
+         self.title=title
+         self.author=author
+         self.pages=pages
+         
+     def __str__(self) -> str:
+          return f"{self.title}, {self.author}, {self.pages}" 
+      
+      
+      
+class Library:
+    def __init__(self):
+        self.books = []
+        
+        
+    def add_book(self, book):
+       
+       self.books.append(book)
+       
+    def list_books(self):
+        for book in self.books:
+            print(book)
+        
