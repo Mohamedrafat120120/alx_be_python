968855a5ceea084d2eead4e1c64dad3f8abab9f7
Mohamed Elbahy <mohamedelbahy10@gmail.com>
2024-10-14 02:26:12 +0300

    1. Mastering Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -27,7 +27,7 @@ class PrintBook(Book):
          self.page_count=page_count
          
      def __str__(self) -> str:
-          return f"PrintBook: {self.title}, {self.author}, Page Count: {self.page_count} " 
+          return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count} " 
       
       
       
